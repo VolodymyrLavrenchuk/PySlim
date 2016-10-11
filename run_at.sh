96ccf0420e027478f7102fae6ea3c93fd78b45e5
mikeplavsky <mikhail.plavskiy@software.dell.com>
2016-10-11 18:13:28 +0000

    running fitnesse

--- run_at.sh
+++ run_at.sh
@@ -3,6 +3,7 @@ NAME=py_slim_fitnesse
 
 docker rm -f $NAME
 docker run -d \
+    -v $(pwd)/tests:/FitNesseRoot \
     --name=$NAME \
-    -p 8082:3680 \
+    -p 8081:8081 \
     mikeplavsky/docker-waferslim 
