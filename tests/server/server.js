34022f071ed8e85e44660ae40bf52e125a8227ca
mikeplavsky <mikhail.plavskiy@software.dell.com>
2016-10-14 09:28:20 +0000

    started waiting but it is yellow now

--- tests/server/server.js
+++ tests/server/server.js
@@ -11,8 +11,37 @@ let middlewares = srv.defaults();
 server.use(middlewares);
 
 server.post("/delete/users", (req, res)=>{
-    console.log(router.db.set('users',[]));
-    res.jsonp({Done:true});
+
+    let r = router.db.set('users',[]);
+    console.log(r);
+
+    res.json({Done:true});
+
+});       
+
+server.post("/create/user", (req,res)=>{
+
+    setTimeout(()=>{
+
+        var res = router.db.set('users',[]);
+        console.log(res);
+
+        res = router.db.get('users').push(
+                req.body);
+        console.log(res);
+
+    },2000);
+
+    res.json(req.body);
+
+});
+
+server.get("/result", (req, res)=>{
+
+    setTimeout(()=>{
+        res.json({Done:true});
+    },3000);
+
 });
 
 server.use(router);
