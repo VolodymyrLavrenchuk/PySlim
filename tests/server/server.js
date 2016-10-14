9739d32d8819d7e4bcb33ea22b592e5407adcdce
mikeplavsky <mikhail.plavskiy@software.dell.com>
2016-10-14 18:47:21 +0000

    fixed fragility when changing attribute

--- tests/server/server.js
+++ tests/server/server.js
@@ -19,15 +19,30 @@ server.post("/delete/users", (req, res)=>{
 
 });       
 
+server.put("/change/user", (req,res)=>{
+
+    setTimeout(()=>{
+
+        res = router.db
+              .get('users')
+              .find({id: req.body.id})
+              .assign(req.body);
+
+        console.log(res);
+
+    },2000);
+
+    res.json(req.body);
+
+});
+
 server.post("/create/user", (req,res)=>{
 
     setTimeout(()=>{
 
-        var res = router.db.set('users',[]);
-        console.log(res);
-
         res = router.db.get('users').push(
                 req.body);
+
         console.log(res);
 
     },2000);
