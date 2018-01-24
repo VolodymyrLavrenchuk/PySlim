8483f66574a18a772731c5323bc4d133498278e0
AleksandrIakhnev <Aleksandr.Iakhnev@quest.com>
2018-01-24 09:39:21 +0000

    Add test

--- tests/toxic_proxy/run.sh
+++ tests/toxic_proxy/run.sh
@@ -10,3 +10,10 @@ docker run -d \
     --network=$NETWORK_NAME \
     -v $(pwd):/$NAME \
     --name=$NAME $NAME 
+
+
+echo "Configuring proxy"
+
+
+docker exec  -t $NAME ./go/bin/toxiproxy-cli create jsonserver -l localhost:4000 -u localhost:3000
+
