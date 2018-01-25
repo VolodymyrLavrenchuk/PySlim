241418bea85bf00fd5a0642cdad9d66c673e6400
AleksandrIakhnev <Aleksandr.Iakhnev@quest.com>
2018-01-25 11:28:15 +0300

    Working on

--- tests/toxy/run.sh
+++ tests/toxy/run.sh
@@ -1,6 +1,6 @@
 NETWORK_NAME=py_slim_server
 
-NAME=toxic_proxy
+NAME=toxy
 
 docker build -t $NAME .
 docker rm -f $NAME
@@ -9,10 +9,13 @@ docker run -d \
     -v $(pwd):/$NAME \
     --network=$NETWORK_NAME \
     -v $(pwd):/$NAME \
-    --name=$NAME $NAME 
+    --name=$NAME $NAME
 
 
-echo "Configuring proxy"
+sleep 2
+
+docker logs toxy
+#echo "Configuring proxy"
 
 
 #docker exec  -t $NAME ./go/bin/toxiproxy-cli create jsonserver -l $NAME:4000 -u py_slim_server:3000
