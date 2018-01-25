2a3c83ae428c8d3ed889a223126046fd05fba677
AleksandrIakhnev <Aleksandr.Iakhnev@quest.com>
2018-01-25 14:52:15 +0300

    clean up

--- tests/toxy/injectError.js
+++ tests/toxy/injectError.js
@@ -5,6 +5,7 @@ const rules = proxy.rules
 const poisons = proxy.poisons
 
 proxy
+  .all('/users')
   .forward('http://py_slim_server:3000')
   .poison(poisons.inject({
     code: 503,
@@ -14,7 +15,16 @@ proxy
   //.withRule(rules.method('GET'))  all requests are poisoned
   .withRule(rules.probability(70))
 
-proxy.all('/*')
+//proxy.all('/*')
+
+proxy
+  .all('/create/user')
+  .poison(poisons.inject({
+    code: 503,
+    body: '{"error": "toxy injected error"}',
+    headers: {'Content-Type': 'application/json'}
+  }))
+  .withRule(rules.probability(100))
 
 proxy.listen(4000)
 console.log('Server listening on port:', 4000)
